4bb9eeea22ec569ace23ca165b2d6cd6dbb451ef
Shea Bunge <bungeshea@gmail.com>
2018-05-17 00:21:27 +1000

    Tidy graph code

--- graph.c
+++ graph.c
@@ -29,7 +29,7 @@ Graph create_graph(int vertices) {
  * @param weight  edge weight
  */
 void add_edge(Graph *self, int from, int dest, int weight) {
-	EdgeNodePtr node = malloc(sizeof(EdgeNodePtr));
+	EdgeNodePtr node = malloc(sizeof(node));
 
 	node->edge.to_vertex = dest;
 	node->edge.weight = weight;
@@ -59,21 +59,21 @@ void destroy_graph(Graph *self) {
 
 /**
  * Run Dijkstra's algorithm on a graph
- * @param graph   instance of Graph
+ * @param self    Graph instance
  * @param source  vertex number to search from
- * @param dist    empty array of length graph->V for storing shortest path distances
- * @param prev    empty array of length graph->V for storing the previous vertex in the paths
+ * @param dist    empty array of length self->V for storing shortest path distances
+ * @param prev    empty array of length self->V for storing the previous vertex in the paths
  */
-void dijkstra(Graph *graph, int source, int *dist, int *prev) {
+void dijkstra(Graph *self, int source, int *dist, int *prev) {
 
 	// Instead of using sets, use an array to determine if each node has been visited
-	int *unvisited = malloc(sizeof(bool) * graph->V);
+	int *unvisited = malloc(sizeof(bool) * self->V);
 
 	// Also keep a running count of how many nodes have been visited
 	int visits = 0;
 
 	// Initialise the arrays with default values
-	for (int v = 0; v < graph->V; v++) {
+	for (int v = 0; v < self->V; v++) {
 		dist[v] = INT_MAX;
 		prev[v] = -1;
 		unvisited[v] = true;
@@ -83,12 +83,12 @@ void dijkstra(Graph *graph, int source, int *dist, int *prev) {
 	dist[source] = 0;
 
 	// Continue looping until all vertices have been visited
-	while (visits < graph->V) {
+	while (visits < self->V) {
 
 		// Find the unvisited vertex with the shortest recorded distance
 		int u = -1;
 
-		for (int v = 0; v < graph->V; v++) {
+		for (int v = 0; v < self->V; v++) {
 			if (unvisited[v] && (u == -1 || dist[v] < dist[u])) {
 				u = v;
 			}
@@ -99,7 +99,7 @@ void dijkstra(Graph *graph, int source, int *dist, int *prev) {
 		visits++;
 
 		// Loop through all outgoing edges of this vertex
-		EdgeNodePtr current = graph->edges[u].head;
+		EdgeNodePtr current = self->edges[u].head;
 		while (current != NULL) {
 			int alt = dist[u] + current->edge.weight;
 			int dest = current->edge.to_vertex;
@@ -139,14 +139,13 @@ int *find_shortest_path(Graph *self, int source, int target) {
 	int length = 0;
 	int vertex = target;
 
-	// continue looping until the previous vertex is undefined (source vertex reached)
 	while (vertex != -1) {
 		path[length] = vertex;
 		vertex = prev[vertex];
 		length++;
 	}
 
-	// create an array more appropriately sized for the final path
+	// create a more appropriately sized array for the final path
 	int *final = malloc(sizeof(int) * (length + 1));
 	final[0] = length; // set the first index to the path length
 
