fbec02104ac137e7a7364c6f7dd778904da042d1
Shea Bunge <bungeshea@gmail.com>
2018-05-16 23:27:53 +1000

    Add comments to graph struct definitions

--- graph.h
+++ graph.h
@@ -1,24 +1,35 @@
 #pragma once
 
+/**
+ * Represents an edge in a graph
+ */
 typedef struct edge {
 	int to_vertex;
 	int weight;
 } Edge;
 
+/**
+ * Represents a linked list node containing a graph edge
+ */
 typedef struct edgeNode {
 	Edge edge;
 	struct edgeNode *next;
 } *EdgeNodePtr;
 
+/**
+ * Represents a linked list of edges
+ */
 typedef struct edgeList {
 	EdgeNodePtr head;
 } EdgeList;
 
+/**
+ * Represents a graph with vertices and edges
+ */
 typedef struct graph {
 	int V;
 	EdgeList *edges;
 } Graph;
-
 
 /**
  * Create a new instance of the graph type
